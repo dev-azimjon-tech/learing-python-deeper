a618dc3eee45eae1d1d0989d5096305fe8c3d984
Azimjon Sobirov <sobirovazimjon02@gmail.com>
2025-07-28 18:20:31 +0800

    Loops

---
+++ loops/learn_main.py
@@ -0,0 +1,35 @@
+# # Types of Loops in Python
+# # for loop - iterates over a sequence(list,tuple,string , etc.)
+# # while loop - repeats as long as a condtion is True
+
+
+# # For loop:
+# # Basic syntax:
+# # for item in itarable:
+# #     pass
+#     # do something
+
+
+# # Code example:
+# for num in [1,2,3]:
+#     print(num)
+
+
+# # Looping with range:
+
+# for i in range(5):
+#     print(i)
+
+# # or
+
+# for j in range(1,6):
+#     print(j)
+
+
+
+# Code example for while loop:
+# count = 0
+
+# while count < 6:
+#     print(count)
+#     count += 1
---
+++ loops/tasks_main.py
@@ -0,0 +1,5 @@
+# Task 1:
+for i in range(0, 10):
+    i += 1
+    print(i)
+
