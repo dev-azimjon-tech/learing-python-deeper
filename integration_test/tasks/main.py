39c45983f99dd6e617198477cda9f84b1b8f77f4
Azimjon Sobirov <sobirovazimjon02@gmail.com>
2025-08-16 11:28:54 +0800

    Did some tasks for integration testing

--- integration_test/tasks/main.py
+++ integration_test/tasks/main.py
@@ -18,22 +18,60 @@
 
 # Task 2:
 
-library = []
+# library = []
 
-def add_book(title):
-    library.append({"title": title, "available": True})
+# def add_book(title):
+#     library.append({"title": title, "available": True})
 
-def borrow_book(title):
-    for book in library:
-        if book["title"] == title and book["available"]:
-            book["available"] = False
-            return True
-    return False
+# def borrow_book(title):
+#     for book in library:
+#         if book["title"] == title and book["available"]:
+#             book["available"] = False
+#             return True
+#     return False
 
-def search_book(title):
-    return any(book["title"] == title for book in library)
+# def search_book(title):
+#     return any(book["title"] == title for book in library)
 
 
-add_book("Python 101")
-borrow_book("Python 101")
-print("Book available after borrow?", search_book("Python 101"))
+# add_book("Python 101")
+# borrow_book("Python 101")
+# print("Book available after borrow?", search_book("Python 101"))
+
+
+# Task 3:
+
+# def plus(num1, num2):
+#     return num1 + num2
+
+# def minus(num1, num2):
+#     return num1 - num2
+
+# def multiply(num1 ,num2):
+#     return num1 * num2
+
+# def divide(num1 ,num2):
+#     return num1 / num2
+
+# print(plus(20,90))
+# print(minus(20,90))
+# print(multiply(20,90))
+# print(divide(20,90))
+
+
+# Task 4: 
+# def main():
+#     name = input("Enter your name: ")
+#     print(f"Hello, {name}!")
+
+
+# Task 5:
+# def main():
+#     username = input("Enter username: ")
+#     password = input("Enter password: ")
+
+#     if username == "admin" and password == "1234":
+#         print("Welcome")
+#     else:
+#         print("Wrong login")
+
--- integration_test/tasks/test_integration_main.py
+++ integration_test/tasks/test_integration_main.py
@@ -21,3 +21,42 @@ from main import *
 
 # def test_search_book():
 #     search = any(book["title"] == book for book in library)
+
+# Task 3 :
+
+# def test_plus():
+#     n1 = 90
+#     n2 = 10
+#     result = plus(n1, n2)
+
+# def test_minus():
+#     n1 = 90
+#     n2 = 10
+#     result = minus(n1, n2)
+    
+# def test_multiply():
+#     n1 = 90
+#     n2 = 10
+#     result = multiply(n1, n2)
+
+# def test_divide():
+#     n1 = 90
+#     n2 = 10
+#     result = divide(n1, n2)    
+
+
+# Task 4:
+# def test_main():
+#     name = "Azimjon"
+#     result = f"Hello, {name}!"
+
+
+# Task 5:
+# def test_login():
+#     username = "aza11"
+#     password = "azaz_20099"
+
+#     if username == "admin" and password == "1234":
+#         assert "Welcome"
+#     else:
+#         assert "You are not ADMIN . WRONG LOGIN"
