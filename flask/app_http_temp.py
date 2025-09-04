fc6f51f89ec0f1501ac5bb110924a36dc84c8ed4
Azimjon Sobirov <sobirovazimjon02@gmail.com>
2025-09-05 07:17:34 +0800

    Did some tasks about HTTP Methods

--- flask/app_http_temp.py
+++ flask/app_http_temp.py
@@ -1,20 +1,22 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route("/", methods=["GET"])
 def home():
     return render_template("home_http.html")
 
-
-@app.route("/form", methods=["GET","POST"])
+@app.route("/form", methods=["GET", "POST"])
 def form():
     if request.method == "POST":
         name = request.form.get("name")
-        return render_template("form_http.html", name=name)
-    return render_template("form_http.html", name=None) 
+        age = request.form.get("age")
+        return render_template("form_http.html", name=name, age=age)
+    return render_template("form_http.html", name=None, age=None)  # fixed "age" here
 
+@app.route("/about", methods=["GET"])
+def about():
+    return "<h1>About Page</h1><p>This is a simple About page.</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True)  
+    app.run(debug=True)
